25e0969f768cb05598fd0b1b99b13bdcb8abb0c4
Lucas Weber <weberl48@outlook.com>
2015-09-15 13:45:21 -0600

    security added, public and private tasks, database abstracted

--- meteor/meteor-todo/meteor-todo.js
+++ meteor/meteor-todo/meteor-todo.js
@@ -13,9 +13,22 @@
 // {{text}} inside of #each block will display each index text property
 
 Tasks = new Mongo.Collection("tasks");
+if (Meteor.isServer) {
+  // This code only runs on the server
 
+  // Only publish tasks that are public or belong to the current user
+  Meteor.publish("tasks", function () {
+    return Tasks.find({
+      $or: [
+        { private: {$ne: true} },
+        { owner: this.userId }
+      ]
+    });
+  });
+}
 if (Meteor.isClient) {
   // This code only runs on the client
+  Meteor.subscribe("tasks");
   Template.body.helpers({
     tasks: function() {
       if (Session.get("hideCompleted")) {
@@ -62,12 +75,7 @@ if (Meteor.isClient) {
       var text = event.target.text.value;
 
       // Insert a task into the collection
-      Tasks.insert({
-        text: text,
-        createdAt: new Date(),            // current time
-        owner: Meteor.userId(),           // _id of logged in user
-        username: Meteor.user().username  // username of logged in user
-      });
+      Meteor.call("addTask", text);
 
       // Clear form
       event.target.text.value = "";
@@ -89,25 +97,69 @@ if (Meteor.isClient) {
   // event.target.text.value = "", is clearing input for new entrys
   // session is a reactive data store for the client
   // - convienet place to store temporary UI state
+  Template.task.helpers({
+     isOwner: function () {
+       return this.owner === Meteor.userId();
+     }
+   });
+
   Template.task.events({
     "click .toggle-checked": function() {
       // Set the checked property to the opposite of its current value
-      Tasks.update(this._id, {
-        $set: {
-          checked: !this.checked
-        }
-      });
+   Meteor.call("setChecked", this._id, ! this.checked);
     },
     "click .delete": function() {
-      Tasks.remove(this._id);
-    }
+      Meteor.call("deleteTask", this._id);
+    },
+      "click .toggle-private": function () {
+        Meteor.call("setPrivate", this._id, ! this.private);
+      }
   });
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
   });
 }
+Meteor.methods({
+  addTask: function (text) {
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
 
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),            // current time
+      owner: Meteor.userId(),           // _id of logged in user
+      username: Meteor.user().username  // username of logged in user
+    });
+  },
+  deleteTask: function (taskId) {
+    var task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can delete it
+      throw new Meteor.Error("not-authorized");
+    }
+    Tasks.remove(taskId);
+  },
+  setChecked: function (taskId, setChecked) {
+    var task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can check it off
+      throw new Meteor.Error("not-authorized");
+    }
+    Tasks.update(taskId, { $set: { checked: setChecked} });
+  },
+  setPrivate: function (taskId, setToPrivate) {
+    var task = Tasks.findOne(taskId);
 
+    // Make sure only the task owner can make a task private
+    if (task.owner !== Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.update(taskId, { $set: { private: setToPrivate } });
+}
+})
 // this refers to an individual task object
 // _id - every inserted document has a unique _id.
 
@@ -120,3 +172,8 @@ if (Meteor.isClient) {
   // - {{currentUser.username}} will display the logged in user's username
   // in javascript user Meteor.userID() to get current user's _id
         //Meteor.user() to get whole user document
+
+//Meteor methods:
+  // When we insert tasks into the database, we can now securely verify that the user is logged in, that the createdAt field is correct, and that the owner and username fields are correct and the user isn't impersonating anyone.
+  // We can add extra validation logic to setChecked and deleteTask in later steps when users can make tasks private.
+  // Our client code is now more separated from our database logic. Instead of a lot of stuff happening inside our event handlers, we now have methods that can be called from anywhere.
