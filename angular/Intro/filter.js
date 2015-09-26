b396cb73274c5912e5d744b14138c39c15387eff
Lucas Weber <weberl48@outlook.com>
2015-09-25 20:21:57 -0600

    custom filter

---
+++ angular/Intro/filter.js
@@ -0,0 +1,17 @@
+function TestCtrl() {
+  var self = this;
+  self.myString = "hello world"
+}
+// this is where the filter magic happens
+function CapitalizeFilter(){
+  // this is the function that Angular will execute wehn the expression is evaludated
+  return function (text) {
+    // test is the original string output of the Angular expression
+    return text.toUpperCase()
+    // and we simply return the text in upper case
+  }
+}
+var app= angular.module('app', [])
+app.controller('TestCtrl', TestCtrl)
+// define a filter called 'capitalize' that will invoke the CapitalizeFilter function
+app.filter('capitalize', CapitalizeFilter);
